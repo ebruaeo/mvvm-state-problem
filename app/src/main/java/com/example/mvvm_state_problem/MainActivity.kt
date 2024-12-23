2ffab1903126aa77c3b990b808ca7c209f8ef3c5
ebruaeo <ebruaeo@hotmail.com>
2024-12-23 15:10:18 +0300

    when we rotate data disappears

---
+++ app/src/main/java/com/example/mvvm_state_problem/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mvvm_state_problem
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.mvvm_state_problem.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private var counter = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.incrementButton.setOnClickListener {
+            counter++
+            binding.counterTV.text = counter.toString()
+        }
+    }
+}
